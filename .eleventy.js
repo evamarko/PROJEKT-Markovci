ce165200d92618d04560930adac04deda4c339f6
Eva Marko <valentova.eva92@gmail.com>
2023-09-26 22:00:36 +0200

    Hotový článek Roadtrip po Severním ostrově Nového Zélandu

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("robots.txt");
 
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
+
   eleventyConfig.addPassthroughCopy("js");
 
   return {
